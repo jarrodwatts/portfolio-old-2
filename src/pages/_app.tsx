c1741945850727461f6149d968ff6d359dec2658
Jarrod Watts <jarrodwatts16@gmail.com>
2023-01-11 22:05:42 +1100

    more like headnah

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import createEmotionCache from "../lib/mui/createEmotionCache";
 import theme from "../lib/mui/theme";
-import Header from "../components/Header/Header";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -25,7 +24,6 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Header />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
